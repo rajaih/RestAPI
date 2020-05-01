eeb6dfc4c418f71db42b863a249d945bb7449af1
rajaih <abha_warudkar@yahoo.com>
2020-05-01 20:05:46 +0530

    TESTING

--- src/DemoPackage/AddPlace.java
+++ src/DemoPackage/AddPlace.java
@@ -20,7 +20,7 @@ public class AddPlace {
 	          .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
 	          .header("Server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
 	System.out.println(resbody);
-		
+		System.out.println("hello");
 
 	}
 
